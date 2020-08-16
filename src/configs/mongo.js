6af20ad8ef648fa427fd5991e3bcd1533f559783
Lucas Monteiro Inacio <lmsupport@outlook.com>
2020-08-16 20:12:13 -0300

    feat: mongo connector class

---
+++ src/configs/mongo.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+class Database {
+    constructor() {
+        this.mongo();
+    }
+
+    mongo() {
+        this.mongoConnection = mongoose.connect(
+            process.env.MONGO_URL,{
+                useNewUrlParser: true,
+                useFindAndModify: true,
+                useUnifiedTopology: true,
+            },
+        );
+    }
+}
+
+module.exports = new Database();
