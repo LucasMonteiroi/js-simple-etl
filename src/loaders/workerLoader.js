10f2a84b74444e93f350ee02d046b86edd05c777
Lucas Monteiro Inacio <lmsupport@outlook.com>
2020-08-16 22:53:40 -0300

    feat: initial loader to run etl object

---
+++ src/loaders/workerLoader.js
@@ -0,0 +1,35 @@
+// const databaseService = require('../services/databaseService');
+// const requestService = require('../services/requestService');
+
+class WorkerLoader { 
+  async run(etl) {
+    if(etl.typeFrom == 'sql' && etl.typeTo == 'sql') { 
+      return await this.runFromSqlToSql(etl, etl.useBearerAuth);
+    } else if (etl.typeFrom == 'sql' && etl.typeTo == 'rest') { 
+      return await this.runFromSqlToApi(etl, etl.useBearerAuth);
+    } else if(etl.typeFrom == 'rest' && etl.typeTo == 'sql') { 
+      return await this.runFromApiToSql(etl, etl.useBearerAuth);
+    } else if(etl.typeFrom == 'rest' && etl.typeTo == 'rest') { 
+      return await this.runFromApiToApi(etl, etl.useBearerAuth);
+    }
+  }
+
+  async runFromSqlToSql(etl, useBearerAuth = false) {
+    return { status: 'Running', description: 'Running from sql to sql', data: etl, useBearerToken: useBearerAuth, percent: 40 };
+  }
+
+  async runFromSqlToApi(etl, useBearerAuth = false) {
+    return { status: 'Running', description: 'Running from sql to rest', data: etl, useBearerToken: useBearerAuth, percent: 40 };
+  }
+
+  async runFromApiToSql(etl, useBearerAuth = false) {
+    return { status: 'Running', description: 'Running from rest to sql', data: etl, useBearerToken: useBearerAuth, percent: 40 };
+  }
+
+  async runFromApiToApi(etl, useBearerAuth = false) {
+    return { status: 'Running', description: 'Running from rest to rest', data: etl, useBearerToken: useBearerAuth, percent: 40 };
+  }
+
+}
+
+module.exports = new WorkerLoader();
