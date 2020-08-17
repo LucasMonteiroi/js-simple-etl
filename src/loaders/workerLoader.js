830503a4d01a7e0f6f374cb647fb4154c31bfa75
Lucas Monteiro Inacio <lmsupport@outlook.com>
2020-08-17 20:33:09 -0300

    feat: get column name and try to pass dynamic data

--- src/loaders/workerLoader.js
+++ src/loaders/workerLoader.js
@@ -1,35 +1,77 @@
-// const databaseService = require('../services/databaseService');
-// const requestService = require('../services/requestService');
+const databaseService = require('../services/databaseService');
+//const requestService = require('../services/requestService');
 
-class WorkerLoader { 
+class WorkerLoader {
   async run(etl) {
-    if(etl.typeFrom == 'sql' && etl.typeTo == 'sql') { 
+    if (etl.typeFrom == 'sql' && etl.typeTo == 'sql') {
       return await this.runFromSqlToSql(etl, etl.useBearerAuth);
-    } else if (etl.typeFrom == 'sql' && etl.typeTo == 'rest') { 
+    } else if (etl.typeFrom == 'sql' && etl.typeTo == 'rest') {
       return await this.runFromSqlToApi(etl, etl.useBearerAuth);
-    } else if(etl.typeFrom == 'rest' && etl.typeTo == 'sql') { 
+    } else if (etl.typeFrom == 'rest' && etl.typeTo == 'sql') {
       return await this.runFromApiToSql(etl, etl.useBearerAuth);
-    } else if(etl.typeFrom == 'rest' && etl.typeTo == 'rest') { 
+    } else if (etl.typeFrom == 'rest' && etl.typeTo == 'rest') {
       return await this.runFromApiToApi(etl, etl.useBearerAuth);
     }
   }
 
   async runFromSqlToSql(etl, useBearerAuth = false) {
-    return { status: 'Running', description: 'Running from sql to sql', data: etl, useBearerToken: useBearerAuth, percent: 40 };
+    return {
+      status: 'Running',
+      description: 'Running from sql to sql',
+      data: etl,
+      useBearerToken: useBearerAuth,
+      percent: 40,
+    };
   }
 
   async runFromSqlToApi(etl, useBearerAuth = false) {
-    return { status: 'Running', description: 'Running from sql to rest', data: etl, useBearerToken: useBearerAuth, percent: 40 };
+    const sqlResult = await databaseService.getData(
+      etl.dataAccess,
+      etl.dataFrom
+    );
+    const columns = sqlResult.map((item) => {
+      return Object.getOwnPropertyNames(item);
+    });
+    const formattedReturns = [];
+    Object.getOwnPropertyNames(etl.dataTo).map((item) => {
+      return columns.map((column) => {
+        return column.map((columnName) => {
+          return formattedReturns.push(`${item}: ${sqlResult[0][columnName]}`);
+        });
+      });
+    });
+
+    return {
+      status: 'Running',
+      description: 'Running from sql to rest',
+      useBearerToken: useBearerAuth,
+      percent: 40,
+      sqlResult,
+      resultColumns: columns,
+      etl,
+      formattedReturns,
+    };
   }
 
   async runFromApiToSql(etl, useBearerAuth = false) {
-    return { status: 'Running', description: 'Running from rest to sql', data: etl, useBearerToken: useBearerAuth, percent: 40 };
+    return {
+      status: 'Running',
+      description: 'Running from rest to sql',
+      data: etl,
+      useBearerToken: useBearerAuth,
+      percent: 40,
+    };
   }
 
   async runFromApiToApi(etl, useBearerAuth = false) {
-    return { status: 'Running', description: 'Running from rest to rest', data: etl, useBearerToken: useBearerAuth, percent: 40 };
+    return {
+      status: 'Running',
+      description: 'Running from rest to rest',
+      data: etl,
+      useBearerToken: useBearerAuth,
+      percent: 40,
+    };
   }
-
 }
 
 module.exports = new WorkerLoader();
